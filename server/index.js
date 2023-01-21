a3ce7e07cdea38bbce41d5b2798bd448eeaf43f2
divsam <sammojaverian@gmail.com>
2023-01-21 12:10:17 -0500

    express connected to mongo

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
 const app = express();
 const port = 3001;
+
+const uri = "mongodb://localhost:27017";
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
