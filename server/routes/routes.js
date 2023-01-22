de9425160e395bb68ba2e85806a8e20d786ca49d
divsam <sammojaverian@gmail.com>
2023-01-21 19:24:11 -0500

    db connected

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,12 +1,24 @@
 const express = require("express");
-
+const Model = require("../model/model");
 const router = express.Router();
 
 module.exports = router;
 
 //Post Method
-router.post("/post", (req, res) => {
-  res.send("Post API");
+router.post("/post", async (req, res) => {
+  const data = new Model({
+    name: req.body.name,
+    age: req.body.age,
+    location: req.body.location,
+    disease: req.body.disease,
+    verified: req.body.verified,
+  });
+  try {
+    const dataToSave = await data.save();
+    res.status(200).json(dataToSave);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 //Get all Method
